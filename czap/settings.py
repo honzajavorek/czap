b6d496bb4e359e8cef7745fc14f4cb274c1da051
Honza Javorek <mail@honzajavorek.cz>
2025-04-13 12:43:46 +0200

    fix https://github.com/honzajavorek/czap/issues/4

--- czap/settings.py
+++ czap/settings.py
@@ -8,6 +8,10 @@ DOWNLOAD_DELAY = 1
 
 AUTOTHROTTLE_ENABLED = True
 
+HTTPCACHE_ENABLED = True
+
+HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 24  # 1 day (in seconds)
+
 FEED_EXPORTERS = {
     "sorted_json": "czap.exporters.SortedJsonItemExporter",
 }
@@ -15,10 +19,18 @@ FEED_EXPORTERS = {
 FEEDS = {
     "items.json": {
         "format": "sorted_json",
-        "encoding": "utf-8",
+        "encoding": "utf8",
         "indent": 4,
+        "overwrite": True,
+    },
+    "items.csv": {
+        "format": "csv",
         "overwrite": True,
     },
 }
 
 DISKCACHE_DIR = ".cache"
+
+DISKCACHE_EXPIRATION_SECS = 60 * 60 * 24  # 1 day (in seconds)
+
+CLOSESPIDER_ERRORCOUNT = 1
--- czap/spiders.py
+++ czap/spiders.py
@@ -27,7 +27,10 @@ class CZAPSpider(scrapy.Spider):
         text = response.text[9:]  # remove "while(1);" from the beginning
         json_structure = json.loads(text)["JsonStructure"]
 
-        with Cache(self.settings["DISKCACHE_DIR"]) as cache:
+        cache_dir = self.settings["DISKCACHE_DIR"]
+        cache_expire = self.settings["DISKCACHE_EXPIRATION_SECS"]
+
+        with Cache(cache_dir) as cache:
             try:
                 data = cast(dict, cache["data"])
                 self.logger.info("Loaded demjson3 data from cache")
@@ -39,29 +42,57 @@ class CZAPSpider(scrapy.Spider):
                 t = threading.Thread(target=animate, args=(holder,))
                 t.start()
                 data = cast(dict, demjson3.decode(json_structure))  # super super slow
-                cache.set("data", data, expire=60 * 60 * 24)  # 1 day (in seconds)
+                cache.set("data", data, expire=cache_expire)
                 holder["done"] = True
 
         members, ids = data["members"]
+        self.logger.info(f"Processing {len(members)} members")
         for i, member in enumerate(members):
             member_id = ids[i]
-            item = {
-                "id": member_id,
-                "url": f"https://czap.cz/Sys/PublicProfile/{member_id}/5485226",
-                "name": html.unescape(member["c1"][0]["v"]),
-                "membership": html.unescape(member["c1"][2]["v"]),
-                "location": html.unescape(member["c2"][0]["v"]),
-                "availability": html.unescape(member["c3"][0]["v"]),
-            }
+            member_url = f"https://czap.cz/Sys/PublicProfile/{member_id}/5485226"
             yield scrapy.Request(
-                item["url"], self.parse_member, cb_kwargs=dict(item=item)
+                member_url,
+                self.parse_member,
+                cb_kwargs=dict(member_id=ids[i], member=member),
             )
 
-    def parse_member(self, response, item):
-        location = response.css('[id*="_TextBoxLabel12682798"]::text').get()
-        item["location"] = f'{location}, {item["location"]}'
-        item["map_url"] = f"https://mapy.cz/zakladni?q={quote(item['location'])}"
-        yield item
+    def parse_member(self, response, member_id, member):
+        city = html.unescape(member["c2"][0]["v"])
+        address = response.css('[id*="_TextBoxLabel12682798"]::text').get()
+        self.logger.debug(f"Location: {city!r} / {address!r}")
+        if is_empty_location(city):
+            location = None
+            mapycom_url = None
+            googlemaps_url = None
+        else:
+            if is_empty_location(address):
+                location = city
+            elif city == address:
+                location = city
+            else:
+                location = f"{address}, {city}"
+            mapycom_url = f"https://mapy.com/zakladni?q={quote(location)}"
+            googlemaps_url = f"https://www.google.com/maps?q={quote(location)}"
+        yield {
+            "id": member_id,
+            "url": response.url,
+            "name": html.unescape(member["c1"][0]["v"]),
+            "membership": html.unescape(member["c1"][2]["v"]),
+            "location": location,
+            "mapycom_url": mapycom_url,
+            "googlemaps_url": googlemaps_url,
+            "availability": html.unescape(member["c3"][0]["v"]),
+        }
+
+
+def is_empty_location(value):
+    if not value:
+        return True
+    if "online" in value.lower() or "on-line" in value.lower():
+        return True
+    if value.lower() in ["na", "n/a", "-", "000", "neuvedeno", "xxx"]:
+        return True
+    return False
 
 
 def animate(holder):
